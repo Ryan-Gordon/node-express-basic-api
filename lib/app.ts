4ed2244c7bcec1aa44e31ed1d6b955d6b8dc858c
Ryan Gordon <ryangordon210@gmail.com>
2018-06-06 22:49:45 +0100

    refactored API to run on HTTPS only
    Generated a cert using openssl
    Didnt commit that

--- lib/app.ts
+++ lib/app.ts
@@ -1,17 +1,28 @@
 // /lib/app.ts
 import * as express from "express";
 import * as bodyParser from "body-parser";
+
+import * as mongoose from "mongoose";
+
 import { Routes } from "./routes/crmRoutes";
 
 class App {
 
     public app: express.Application;
     public routePrv: Routes = new Routes();
+    public mongoUrl: string = 'mongodb://localhost/CRMdb';  
+
     
     constructor() {
         this.app = express();
         this.config();        
-        this.routePrv.routes(this.app);     
+        this.routePrv.routes(this.app);   
+        this.mongoSetup();  
+    }
+
+    private mongoSetup(): void{
+        mongoose.Promise = global.Promise;
+        mongoose.connect(this.mongoUrl);    
     }
 
     private config(): void{
--- lib/server.ts
+++ lib/server.ts
@@ -1,7 +1,30 @@
 // lib/server.ts
 import app from "./app";
+import * as https from 'https';
+import * as fs from 'fs';
 const PORT = 3000;
 
+
+const httpsOptions = {
+    key: fs.readFileSync('./config/key.pem'),
+    cert: fs.readFileSync('./config/cert.pem')
+}
+
+/**
+ * Creating a HTTPS
+ */
+https.createServer(httpsOptions, app).listen(PORT, () => {
+    console.log('Express server listening on port ' + PORT);
+})
+
+
+
+/**
+ * 
+ * Creating a HTTP server
+ 
 app.listen(PORT, () => {
     console.log('Express server listening on port ' + PORT);
 })
+
+*/
